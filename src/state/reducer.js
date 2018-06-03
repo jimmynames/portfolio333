19768f6eef337950a6f2917603e68649a054fd53
Jimmy Names <jim.me.ryan@gmail.com>
2018-06-03 15:46:06 +0100

    re init

---
+++ src/state/reducer.js
@@ -0,0 +1,11 @@
+//taken from the official redux docs
+export default function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+    }
+};
